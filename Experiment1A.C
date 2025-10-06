eeb0e794e3b293b9504d572ee706aa3ec209203c
krushnachavan578-sys <krushnachavan578@gmail.com>
2025-10-06 16:31:09 +0530

    Experiment1a.c

---
+++ Experiment1A.C
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+printf("Name: Krushna Sheshrao chavan\n");
+printf("Age = 21\n");
+printf("Class: Civil\n");
+printf("Division: B\n");
+printf("UID: 251C026\n");
+return 0;
+}
