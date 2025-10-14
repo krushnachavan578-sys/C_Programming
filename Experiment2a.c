94f427f76a947956912bd99b511b5dce599fe0ff
krushnachavan578-sys <krushnachavan578@gmail.com>
2025-10-15 05:24:02 +0530

    Experiment2a.c

---
+++ Experiment2a.c
@@ -0,0 +1,15 @@
+/* name: krushna
+UIN: 251C026
+feild: civil
+div: B */
+#include <stdio.h>
+int main() {
+int num;
+printf("Enter a number: ");
+scanf("%d", &num);
+if (num % 2 == 0)
+printf("%d is Even.\n", num);
+else
+printf("%d is Odd.\n", num);
+return 0;
+}
