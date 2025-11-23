31884fae8bf68f86279a694f0f1e4f45ebc09fcf
krushnachavan578-sys <krushnachavan578@gmail.com>
2025-11-23 12:11:26 +0530

    Experiment5b.c

---
+++ Experiment5b.c
@@ -0,0 +1,25 @@
+/* NAME: krushna sheshrao chavan
+UIN :- 251C026
+CLASS:- CIVIL
+ROLL NO:- 21
+DIV:- B */
+
+#include<stdio.h>
+
+int factorialRecursive(int num)
+{
+    if(num<=1) 
+    {
+        return 1;
+    }
+    return num*factorialRecursive(num-1);
+}
+int main()
+{
+    int num;
+    printf("\t\t\t**Recursive Factorial Function**\n\n");
+    printf("Enter Number= ");
+    scanf("%d",&num);
+    printf("Factorial of %d is %d",num,factorialRecursive(num));
+    return 0;
+}
